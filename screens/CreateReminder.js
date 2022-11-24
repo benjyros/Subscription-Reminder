ae418e641a8db1374562b6706646491de411d146
benjyros <benny.peterhans@gmail.com>
2022-11-24 13:01:20 +0100

    implementation of screen for creating notes

--- screens/CreateReminder.js
+++ screens/CreateReminder.js
@@ -1,16 +1,84 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, TouchableOpacity, Text, View } from 'react-native';
+import { Button, StyleSheet, ScrollView, TouchableOpacity, Text, TextInput, View } from 'react-native';
+import DatePicker from 'react-native-date-picker'
 
-export default function CreateReminder ({ navigation }) {
+export default function CreateReminder({ navigation }) {
+    const [title, onChangeTitle] = React.useState("title");
+    const [description, onChangDescription] = React.useState("description");
+    const [costs, onChangeCosts] = React.useState("CHF");
+
+    const [dateStart, setDateStart] = React.useState(new Date());
+    const [dateEnd, setDateEnd] = React.useState(new Date());
+    const [open, setOpen] = React.useState(false);
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} >
             <View style={styles.header}>
                 <Text style={styles.title}>Create Reminder</Text>
             </View>
-            <View style={styles.main}>
-                <Text></Text>
-            </View>
+            <ScrollView
+                style={styles.main}
+                contentContainerStyle={{ flexGrow: 1 }}
+                keyboardShouldPersistTaps='handled'
+            >
+                <Text>Title of Subscription:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={text => onChangeTitle(text)}
+                    placeholder={"title"}
+                />
+                <Text>Costs:</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="numeric"
+                    placeholder={"CHF"}
+                />
+                <Text>Start:</Text>
+                <TextInput
+                    style={styles.input}
+                    onPressIn={() => setOpen(true)}
+                    value={dateStart}
+                />
+                <DatePicker
+                    modal
+                    open={open}
+                    date={dateStart}
+                    onConfirm={(date) => {
+                        setOpen(false)
+                        setDateStart(date)
+                    }}
+                    onCancel={() => {
+                        setOpen(false)
+                    }}
+                />
+                <Text>End:</Text>
+                <TextInput
+                    style={styles.input}
+                    onPressIn={() => setOpen(true)}
+                    value={dateEnd}
+                />
+                <DatePicker
+                    modal
+                    open={open}
+                    date={dateEnd}
+                    onConfirm={(date) => {
+                        setOpen(false)
+                        setDateEnd(date)
+                    }}
+                    onCancel={() => {
+                        setOpen(false)
+                    }}
+                />
+                <Text>Description:</Text>
+                <TextInput
+                    style={styles.multiInput}
+                    multiline
+                    numberOfLines={5}
+                    onChangeText={text => onChangDescription(text)}
+                    placeholder={"description"}
+                />
+            </ScrollView>
             <View style={styles.buttonView}>
                 <View style={{ width: '50%', paddingRight: 10 }}>
                     <TouchableOpacity
@@ -41,7 +109,6 @@ export default function CreateReminder ({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         backgroundColor: '#efdbcb',
     },
     header: {
@@ -69,12 +136,23 @@ const styles = StyleSheet.create({
         paddingTop: 50,
         paddingHorizontal: 20,
     },
+    input: {
+        height: 40,
+        borderWidth: 1,
+        padding: 10,
+    },
+    multiInput: {
+        height: 200,
+        borderWidth: 1,
+        padding: 10,
+    },
     buttonView: {
         width: '100%',
         alignSelf: 'flex-end',
         padding: 20,
         flexDirection: 'row',
         justifyContent: 'space-evenly',
+        backgroundColor: '#efdbcb',
     },
     button: {
         backgroundColor: '#4bb4de',
